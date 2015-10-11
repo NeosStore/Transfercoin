ca495aefd7ebb82f192151fb7acb1675a362c5ff
Infernoman <jjaauto@hotmail.com>
2015-10-11 15:32:29 -0400

    test fix #1

--- src/main.h
+++ src/main.h
@@ -25,11 +25,7 @@ static const int64_t DARKSEND_COLLATERAL = (10000*COIN);
 static const int64_t DARKSEND_FEE = (0.0001*COIN);
 static const int64_t DARKSEND_POOL_MAX = (100000.99*COIN);
 static const int64_t STATIC_POS_REWARD = 1 * COIN; //Constant reward of 1 TX per COIN i.e. 8% 
-if(nHeight <= 85000){
-    static const int64_t TARGET_SPACING = 60;
-} else {
-    static const int64_t TARGET_SPACING = 69;
-}
+
 
 /*
     At 15 signatures, 1/2 of the masternode network can be owned by
@@ -1083,6 +1079,15 @@ public:
     bool IsProofOfStake() const
     {
         return (nFlags & BLOCK_PROOF_OF_STAKE);
+    }
+
+    bool Spacing() const
+    {
+        if (nHeight <= 85000){
+            return static const int64_t TARGET_SPACING = 60;
+        } else {
+            return static const int64_t TARGET_SPACING = 69;
+        }
     }
 
     void SetProofOfStake()
